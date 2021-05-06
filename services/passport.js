d026bf49fb9e0198555a7ce6cb1ca694f70a164c
Mani Movassagh <mani.mgh@gmail.com>
2021-05-06 08:58:22 +0200

    concurrently

--- services/passport.js
+++ services/passport.js
@@ -20,14 +20,15 @@ passport.deserializeUser((id, done) => {
     done(null, user);
   });
 });
-//
+
 //use passport js to bring google strategy
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
